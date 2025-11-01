ee1272e75970d0536a5b6142057b99f020962b89
HHYYYY <hhyyyy@hhysky.eu.org>
2025-11-01 23:19:59 +0800

    Update Links

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-fetch('/sourse/README.md') // 替换为你的 .md 文件路径
+fetch('/sourse/Links.md') // 替换为你的 .md 文件路径
   .then(response => response.text())
   .then(text => {
     document.getElementById('md-content').innerHTML = marked.parse(text);
